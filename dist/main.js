e3785cdecdfdb4e67b0e6daccbc87bc388e0f7a3
Damián De Lio <damiandelio@gmial.com>
2020-07-03 17:58:42 -0300

    fixs React.Fragment undefined

--- dist/main.js
+++ dist/main.js
@@ -1,208 +1,209 @@
 function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
 
-var React = _interopDefault(require('react'));
+var React = require('react');
+var React__default = _interopDefault(React);
 var ReactDOM = _interopDefault(require('react-dom'));
 require('spinkit/spinkit.min.css');
 require('./main.css');
 
 var PlaneSpinner = function PlaneSpinner() {
-  return /*#__PURE__*/React.createElement("div", {
+  return /*#__PURE__*/React__default.createElement("div", {
     className: "sk-plane"
   });
 };
 var ChaseSpinner = function ChaseSpinner() {
-  return /*#__PURE__*/React.createElement("div", {
+  return /*#__PURE__*/React__default.createElement("div", {
     className: "sk-chase"
-  }, /*#__PURE__*/React.createElement("div", {
+  }, /*#__PURE__*/React__default.createElement("div", {
     className: "sk-chase-dot"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-chase-cot"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-chase-dot"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-chase-dot"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-chase-dot"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-chase-dot"
   }));
 };
 var BaunceSpinner = function BaunceSpinner() {
-  return /*#__PURE__*/React.createElement("div", {
+  return /*#__PURE__*/React__default.createElement("div", {
     className: "sk-bounce"
-  }, /*#__PURE__*/React.createElement("div", {
+  }, /*#__PURE__*/React__default.createElement("div", {
     className: "sk-bounce-dot"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-bounce-dot"
   }));
 };
 var WaveSpinner = function WaveSpinner() {
-  return /*#__PURE__*/React.createElement("div", {
+  return /*#__PURE__*/React__default.createElement("div", {
     className: "sk-wave"
-  }, /*#__PURE__*/React.createElement("div", {
+  }, /*#__PURE__*/React__default.createElement("div", {
     className: "sk-wave-rect"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-wave-rect"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-wave-rect"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-wave-rect"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-wave-rect"
   }));
 };
 var PulseSpinner = function PulseSpinner() {
-  return /*#__PURE__*/React.createElement("div", {
+  return /*#__PURE__*/React__default.createElement("div", {
     className: "sk-pulse"
   });
 };
 var FlowSpinner = function FlowSpinner() {
-  return /*#__PURE__*/React.createElement("div", {
+  return /*#__PURE__*/React__default.createElement("div", {
     className: "sk-flow"
-  }, /*#__PURE__*/React.createElement("div", {
+  }, /*#__PURE__*/React__default.createElement("div", {
     className: "sk-flow-dot"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-flow-dot"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-flow-dot"
   }));
 };
 var SwingSpinner = function SwingSpinner() {
-  return /*#__PURE__*/React.createElement("div", {
+  return /*#__PURE__*/React__default.createElement("div", {
     className: "sk-swing"
-  }, /*#__PURE__*/React.createElement("div", {
+  }, /*#__PURE__*/React__default.createElement("div", {
     className: "sk-swing-dot"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-swing-dot"
   }));
 };
 var CircleSpinner = function CircleSpinner() {
-  return /*#__PURE__*/React.createElement("div", {
+  return /*#__PURE__*/React__default.createElement("div", {
     className: "sk-circle"
-  }, /*#__PURE__*/React.createElement("div", {
+  }, /*#__PURE__*/React__default.createElement("div", {
     className: "sk-circle-dot"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-circle-dot"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-circle-dot"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-circle-dot"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-circle-dot"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-circle-dot"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-circle-dot"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-circle-dot"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-circle-dot"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-circle-dot"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-circle-dot"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-circle-dot"
   }));
 };
 var CircleFadeSpinner = function CircleFadeSpinner() {
-  return /*#__PURE__*/React.createElement("div", {
+  return /*#__PURE__*/React__default.createElement("div", {
     className: "sk-circle-fade"
-  }, /*#__PURE__*/React.createElement("div", {
+  }, /*#__PURE__*/React__default.createElement("div", {
     className: "sk-circle-fade-dot"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-circle-fade-dot"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-circle-fade-dot"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-circle-fade-dot"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-circle-fade-dot"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-circle-fade-dot"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-circle-fade-dot"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-circle-fade-dot"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-circle-fade-dot"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-circle-fade-dot"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-circle-fade-dot"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-circle-fade-dot"
   }));
 };
 var GridSpinner = function GridSpinner() {
-  return /*#__PURE__*/React.createElement("div", {
+  return /*#__PURE__*/React__default.createElement("div", {
     className: "sk-grid"
-  }, /*#__PURE__*/React.createElement("div", {
+  }, /*#__PURE__*/React__default.createElement("div", {
     className: "sk-grid-cube"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-grid-cube"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-grid-cube"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-grid-cube"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-grid-cube"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-grid-cube"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-grid-cube"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-grid-cube"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-grid-cube"
   }));
 };
 var FoldSpinner = function FoldSpinner() {
-  return /*#__PURE__*/React.createElement("div", {
+  return /*#__PURE__*/React__default.createElement("div", {
     className: "sk-fold"
-  }, /*#__PURE__*/React.createElement("div", {
+  }, /*#__PURE__*/React__default.createElement("div", {
     className: "sk-fold-cube"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-fold-cube"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-fold-cube"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-fold-cube"
   }));
 };
 var WanderSpinner = function WanderSpinner() {
-  return /*#__PURE__*/React.createElement("div", {
+  return /*#__PURE__*/React__default.createElement("div", {
     className: "sk-wander"
-  }, /*#__PURE__*/React.createElement("div", {
+  }, /*#__PURE__*/React__default.createElement("div", {
     className: "sk-wander-cube"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-wander-cube"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-wander-cube"
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/React__default.createElement("div", {
     className: "sk-wander-cube"
   }));
 };
 
 var LOADER_ID = "super-react-loader";
-var DEFAULT_LOADER = /*#__PURE__*/React.createElement(CircleFadeSpinner, null);
+var DEFAULT_LOADER = /*#__PURE__*/React__default.createElement(CircleFadeSpinner, null);
 var DEFAULT_SIZE = "4rem";
 var DEFAULT_COLOR = "#333";
 var DEFAULT_BACKGROUND = "#fff";
 var ROOT = document.documentElement;
 var PRESET_LOADERS = {
-  "plane": /*#__PURE__*/React.createElement(PlaneSpinner, null),
-  "chase": /*#__PURE__*/React.createElement(ChaseSpinner, null),
-  "baunce": /*#__PURE__*/React.createElement(BaunceSpinner, null),
-  "wave": /*#__PURE__*/React.createElement(WaveSpinner, null),
-  "pulse": /*#__PURE__*/React.createElement(PulseSpinner, null),
-  "flow": /*#__PURE__*/React.createElement(FlowSpinner, null),
-  "swing": /*#__PURE__*/React.createElement(SwingSpinner, null),
-  "circle": /*#__PURE__*/React.createElement(CircleSpinner, null),
-  "circle-fade": /*#__PURE__*/React.createElement(CircleFadeSpinner, null),
-  "grid": /*#__PURE__*/React.createElement(GridSpinner, null),
-  "fold": /*#__PURE__*/React.createElement(FoldSpinner, null),
-  "wander": /*#__PURE__*/React.createElement(WanderSpinner, null)
+  "plane": /*#__PURE__*/React__default.createElement(PlaneSpinner, null),
+  "chase": /*#__PURE__*/React__default.createElement(ChaseSpinner, null),
+  "baunce": /*#__PURE__*/React__default.createElement(BaunceSpinner, null),
+  "wave": /*#__PURE__*/React__default.createElement(WaveSpinner, null),
+  "pulse": /*#__PURE__*/React__default.createElement(PulseSpinner, null),
+  "flow": /*#__PURE__*/React__default.createElement(FlowSpinner, null),
+  "swing": /*#__PURE__*/React__default.createElement(SwingSpinner, null),
+  "circle": /*#__PURE__*/React__default.createElement(CircleSpinner, null),
+  "circle-fade": /*#__PURE__*/React__default.createElement(CircleFadeSpinner, null),
+  "grid": /*#__PURE__*/React__default.createElement(GridSpinner, null),
+  "fold": /*#__PURE__*/React__default.createElement(FoldSpinner, null),
+  "wander": /*#__PURE__*/React__default.createElement(WanderSpinner, null)
 };
 var Loader = DEFAULT_LOADER;
 var background = DEFAULT_BACKGROUND;
@@ -251,7 +252,7 @@ function show(callback) {
   if (!hasRunForFirstTime) appendChildToDom();
 
   if (count === 0) {
-    ReactDOM.render( /*#__PURE__*/React.createElement("div", {
+    ReactDOM.render( /*#__PURE__*/React__default.createElement("div", {
       style: {
         background: background
       },
@@ -273,7 +274,7 @@ function hide(callback) {
     }
 
     if (count === 0) {
-      ReactDOM.render( /*#__PURE__*/React.createElement(Fragment, null), document.getElementById(LOADER_ID), callback);
+      ReactDOM.render( /*#__PURE__*/React__default.createElement(React.Fragment, null), document.getElementById(LOADER_ID), callback);
     }
   }
 }
